217e8ee43cc75dc05d67cfa6901cc7f437f72d39
Prince sharma <131739429+Prince-Sharma-07@users.noreply.github.com>
2024-07-25 19:32:43 +0530

    Time: 170 ms (54.72%), Space: 98.5 MB (45.68%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,7 +1,20 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        unordered_map<int , int> mpp;
+        for(auto it : nums){
+            mpp[it]++;
+        }
+        int i = 0;
+        int mini = *min_element(nums.begin() , nums.end());
+        int maxi = *max_element(nums.begin() , nums.end());
+        for(int num = mini; num<=maxi; num++){
+            while(mpp[num] > 0){
+                nums[i] = num;
+                i++;
+                mpp[num]--; 
+            }
+        }
         return nums;
     }
 };
