1c86c4c6551d5d3efebee3157b1cee6c03ec10f1
Prince sharma <131739429+Prince-Sharma-07@users.noreply.github.com>
2024-11-07 23:31:52 +0530

    Time: 0 ms (100%), Space: 7.5 MB (54.13%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int tribonacci(int n) {
-         if (n < 3) {
-            return n > 0 ? 1 : 0;
-        }
-        
-        int a = 0, b = 1, c = 1;
-        
-        for (int i = 2; i < n; ++i) {
-            int tmp = a + b + c;
+        if(n<3) return n==0 ? 0 : 1;
+
+        int a = 0 , b = 1 , c = 1;
+
+        for(int i = 3 ; i<=n; i++){
+            int  temp = a + b + c;
             a = b;
             b = c;
-            c = tmp;
+            c = temp;
         }
-        
         return c;
     }
 };
