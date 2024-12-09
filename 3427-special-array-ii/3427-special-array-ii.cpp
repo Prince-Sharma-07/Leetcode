ff53c905f128578a34c8cbac0ae0fea8551b5994
Prince sharma <131739429+Prince-Sharma-07@users.noreply.github.com>
2024-12-09 20:49:05 +0530

    Time: 47 ms (19.92%), Space: 136.5 MB (22.19%) - LeetHub

---
+++ 3427-special-array-ii/3427-special-array-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
+    int n = queries.size();
+    int m = nums.size();
+    vector<bool> ans;
+    vector<int> Prefix(m);
+    Prefix[0] = 0;
+    for(int i = 1; i<nums.size(); i++){
+        if(nums[i]%2 == 1 && nums[i-1]%2 == 1 || nums[i]%2==0 && nums[i-1]%2 == 0) Prefix[i] = Prefix[i-1]+1;
+        else Prefix[i] = Prefix[i-1];  
+    }
+    
+    for(auto it : queries){
+        int start = it[0];
+        int end = it[1];
+        if(Prefix[end] - Prefix[start]) ans.push_back(false);
+        else ans.push_back(true);
+    }
+    return ans;
+    }
+};
