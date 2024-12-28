0235316f96c5a75db12a84086db33fa5b650f868
Prince sharma <131739429+Prince-Sharma-07@users.noreply.github.com>
2024-12-28 10:17:48 +0530

    Time: 0 ms (100%), Space: 16.2 MB (33.22%) - LeetHub

---
+++ 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int gcd(int a , int b){
+        if(b==0) return a;
+        return gcd(b , a%b);
+    }
+    int findGCD(vector<int>& nums) {
+        int maxi = *max_element(nums.begin() , nums.end());
+        int mini = *min_element(nums.begin() , nums.end());
+        return gcd(maxi , mini);
+    }
+};
