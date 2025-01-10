f0bfa2410057dfe2229e6ea893cc34db41c1988e
Prince sharma <131739429+Prince-Sharma-07@users.noreply.github.com>
2025-01-11 00:30:59 +0530

    Time: 159 ms (23.75%), Space: 360.7 MB (5.02%) - LeetHub

--- 0952-word-subsets/0952-word-subsets.cpp
+++ 0952-word-subsets/0952-word-subsets.cpp
@@ -1,30 +1,38 @@
 class Solution {
 public:
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
-        vector<int> freq(256,0);
+        vector<int> Maxfreq(256,0);
         vector<string> ans;
 
         for(auto i : words2){
             vector<int> temp(256,0);
             for(auto j : i){
                 temp[j]++;
-                if(temp[j] > freq[j]){
-                    freq[j] = temp[j];
+                if(temp[j] > Maxfreq[j]){
+                    Maxfreq[j] = temp[j];
                 }
             }
         }
       
         for(auto i : words1){
+
             vector<int> temp(256,0);
             for(auto j : i){
                 temp[j]++;
             }
             
-            int k;
-            for(k = 0; k<temp.size(); k++){
-                if(freq[k] > 0 && freq[k] > temp[k]) break;
+            bool flag = true;
+            for(int k = 0; k<temp.size(); k++){
+
+                if(temp[k] < Maxfreq[k]){
+                 flag = false;
+                 break;
+                 }
+
             }
-            if(k == 256) ans.push_back(i);
+
+            if(flag) ans.push_back(i);
+            
         }
 
         return ans;
