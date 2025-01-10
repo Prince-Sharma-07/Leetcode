a40fdbe099c7136b82512dc9c546d76c18000605
Prince sharma <131739429+Prince-Sharma-07@users.noreply.github.com>
2025-01-10 23:20:13 +0530

    Time: 0 ms (100%), Space: 9.8 MB (13.15%) - LeetHub

--- 1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.cpp
+++ 1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     vector<int> sumZero(int n) {
         vector<int> ans;
-        int sum = 0;
-        for(int i = 1; i<n; i++){
+        
+        for(int i = 1; i<=n/2; i++){
            ans.push_back(i);
-           sum += i;
+           ans.push_back(-i);
         }
-        ans.push_back(-sum);
+
+        if(n&1) ans.push_back(0);
         return ans;
     }
 };
