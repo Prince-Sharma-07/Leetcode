1f270c22f12c9425be366f4d6f8e682142b4d5a8
Prince sharma <131739429+Prince-Sharma-07@users.noreply.github.com>
2025-01-14 15:16:14 +0530

    Time: 7 ms (57.88%), Space: 88.4 MB (38.01%) - LeetHub

--- 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.cpp
+++ 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.cpp
@@ -3,13 +3,11 @@ public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
         unordered_set<int> s;
         vector<int> ans;
-        int sum = 0;
         for(int i = 0; i<A.size(); i++){
            int prevSize = s.size();
            s.insert(A[i]);
            s.insert(B[i]);
-           int diff = s.size()-prevSize;
-           sum += abs(diff-2);
+           int sum = 2*(i+1) - s.size();
            ans.push_back(sum);
         }
         return ans;
