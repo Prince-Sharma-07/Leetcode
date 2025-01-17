ace35e8742a74769d6c65b93eab1307d12d95019
Prince sharma <131739429+Prince-Sharma-07@users.noreply.github.com>
2025-01-17 12:19:01 +0530

    Time: 0 ms (100%), Space: 8.1 MB (18.13%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -2,12 +2,7 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         if(n == 0) return 0;
-        int x = 1;
-        while(x <= n){
-            if(x == n) return 1;
-            if(x > INT_MAX/2) break;
-            x = x << 1;
-        }
-        return 0;
+         
+         return floor(log2(n)) == ceil(log2(n)) ? 1 : 0;
     }
 };
