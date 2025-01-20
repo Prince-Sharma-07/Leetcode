551026cac05f2045d880e9cc32ab2e4c3c886fc8
Prince sharma <131739429+Prince-Sharma-07@users.noreply.github.com>
2025-01-20 18:40:24 +0530

    Time: 54 ms (53.48%), Space: 8 MB (52.41%) - LeetHub

--- 1879-maximum-score-from-removing-stones/1879-maximum-score-from-removing-stones.cpp
+++ 1879-maximum-score-from-removing-stones/1879-maximum-score-from-removing-stones.cpp
@@ -6,18 +6,20 @@ public:
         pq.push(b);
         pq.push(c);
         int count = 0;
+
         while(1){
+            
             int first = pq.top();
             pq.pop();
             int second = pq.top();
             pq.pop();
 
-            if(first > 0 && second > 0){
-                count++;
-                pq.push(--first);
-                pq.push(--second);
-            }
-            else return count;
+            if(first == 0 or second == 0) break;
+
+            count++;
+            pq.push(--first);
+            pq.push(--second);
+            
         }
 
         return count;
