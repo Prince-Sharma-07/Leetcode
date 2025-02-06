58b0e4069a3baada4f28990d47c592b8ca22685c
Prince sharma <131739429+Prince-Sharma-07@users.noreply.github.com>
2025-02-06 21:06:24 +0530

    Time: 2 ms (39.22%), Space: 9.2 MB (22.85%) - LeetHub

---
+++ 0299-bulls-and-cows/0299-bulls-and-cows.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        unordered_map<char , int> mp;
+        int bulls = 0 , cows = 0;
+
+        for(auto i : secret){
+            mp[i]++; 
+        }
+
+        for(int i = 0; i<secret.size(); i++){
+            if(secret[i] == guess[i]){
+                mp[guess[i]]--;
+                bulls++;
+            }
+        }
+        for(int i = 0; i<guess.size(); i++){
+            if(secret[i] != guess[i] && mp[guess[i]]){
+                mp[guess[i]]--;
+                cows++;
+            }
+        }
+
+        string ans;
+        ans += to_string(bulls);
+        ans += "A";
+        ans += to_string(cows);
+        ans += "B";
+        
+        return ans;
+    }
+};
