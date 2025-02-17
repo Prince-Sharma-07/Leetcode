09dfa75f6b324b1d81beea6da462897cfea0f1bd
Prince sharma <131739429+Prince-Sharma-07@users.noreply.github.com>
2025-02-17 22:06:37 +0530

    Time: 0 ms (100%), Space: 8.4 MB (7.69%) - LeetHub

--- 1585-the-kth-factor-of-n/1585-the-kth-factor-of-n.cpp
+++ 1585-the-kth-factor-of-n/1585-the-kth-factor-of-n.cpp
@@ -2,8 +2,18 @@ class Solution {
 public:
     int kthFactor(int n, int k) {
         vector<int> ans;
-        for(int i = 1; i<=n; i++){
-            if(n%i == 0) ans.push_back(i);
+        stack<int> st;
+        for(int i = 1; i*i<=n; i++){
+            if(n%i == 0){
+                ans.push_back(i);
+                st.push(n/i);
+            } 
+        }
+        while(!st.empty()){
+            if(ans.back() != st.top()){
+            ans.push_back(st.top());
+            }
+            st.pop();
         }
         if(k-1 >= ans.size()) return -1;
         return ans[k-1];
