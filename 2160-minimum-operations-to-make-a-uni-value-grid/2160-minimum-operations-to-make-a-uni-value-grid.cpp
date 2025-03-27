d054c45b786d04e057525cbdc6dd7ba0c6088e6a
Prince sharma <131739429+Prince-Sharma-07@users.noreply.github.com>
2025-03-27 22:38:12 +0530

    Time: 62 ms (11.75%), Space: 99.8 MB (6.93%) - LeetHub

--- 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -3,22 +3,21 @@ public:
     int minOperations(vector<vector<int>>& grid, int x) {
         vector<int> temp;
 
-        for(int i  = 0; i<grid.size(); i++){
-            for(int j = 0; j<grid[i].size(); j++){
-                temp.push_back(grid[i][j]);
+        for(auto i : grid){
+            for(auto j : i){
+                temp.push_back(j);
             }
         }
-        
+
         sort(temp.begin() , temp.end());
-        int n = temp.size();
-        int target = temp[n/2] , ans = 0;
-       
+        int n = temp.size() , ans = 0;
 
-       for(int &i : temp){
-         if(i%x != target%x) return -1;
-         ans += abs(target-i)/x;
-       }
-
+        for(int i = 0; i<n; i++){
+            if(i != n/2 ){
+                if((abs(temp[n/2]-temp[i]))%x != 0) return -1;
+                ans += (abs(temp[n/2]-temp[i]))/x;
+            }
+        }
         return ans;
     }
 };
