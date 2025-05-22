da72381189df404c8486860210d1d567d5524885
Prince sharma <131739429+Prince-Sharma-07@users.noreply.github.com>
2025-05-22 14:42:49 +0530

    Time: 4 ms (13.82%), Space: 43.5 MB (95.96%) - LeetHub

--- 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.cpp
+++ 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int maxSubarraySumCircular(vector<int>& nums) {
-         int totalSum = 0;
-        int maxKadane = nums[0], currMax = nums[0];
-        int minKadane = nums[0], currMin = nums[0];
+        int currMaxSum = 0, currMinSum, maxSum = INT_MIN, TotalSum = 0, minSum = INT_MAX;
 
-        for (int i = 1; i < nums.size(); i++) {
-            currMax = max(nums[i], currMax + nums[i]);
-            maxKadane = max(maxKadane, currMax);
+        for (int i = 0; i < nums.size(); i++) {
+            currMaxSum += nums[i];
+            currMinSum += nums[i];
 
-            currMin = min(nums[i], currMin + nums[i]);
-            minKadane = min(minKadane, currMin);
+            maxSum = max(maxSum, currMaxSum);
+            minSum = min(minSum, currMinSum);
+
+            if (currMaxSum < 0)
+                currMaxSum = 0;
+            if (currMinSum > 0)
+                currMinSum = 0;
         }
 
-        totalSum = accumulate(nums.begin(), nums.end(), 0);
+        TotalSum = accumulate(nums.begin(), nums.end(), 0);
 
-       
-        if (maxKadane < 0) return maxKadane;
-
-        return max(maxKadane, totalSum - minKadane);
+        return max(maxSum , TotalSum - minSum);
     }
 };
