e94467c3b53fe551c19f9fb9d152375d7ea5bb9e
Prince sharma <131739429+Prince-Sharma-07@users.noreply.github.com>
2025-07-02 10:46:45 +0530

    Time: 308 ms (14.32%), Space: 197.2 MB (14.92%) - LeetHub

--- 2445-reachable-nodes-with-restrictions/2445-reachable-nodes-with-restrictions.cpp
+++ 2445-reachable-nodes-with-restrictions/2445-reachable-nodes-with-restrictions.cpp
@@ -1,5 +1,11 @@
 class Solution {
 public:
+    void dfs(int node , vector<vector<int>> &adj, vector<bool> &vis, unordered_map<int , bool> &mp){
+        vis[node] = 1;
+        for(auto i : adj[node]){
+            if(!vis[i] && !mp[i]) dfs(i , adj , vis , mp);
+        }
+    }
     int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& restricted) {
         unordered_map<int , bool> mp;
         for(auto i : restricted){
@@ -12,19 +18,9 @@ public:
             adj[i[0]].push_back(i[1]);
             adj[i[1]].push_back(i[0]);
         }
-        queue<int> q;
-        q.push(0);
-        vis[0] = 1;
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            for(auto j : adj[node]){
-                if(!vis[j] && !mp[j]){
-                    q.push(j);
-                    vis[j] = true;
-                }
-            }
-        }
+        
+        dfs(0 , adj , vis , mp);
+
         for(auto i : vis){
             if(i) count++;
         }
