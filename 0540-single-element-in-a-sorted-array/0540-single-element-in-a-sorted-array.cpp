91d24cdb1239cdc25ff64112b99cb6015d8795a2
Prince sharma <131739429+Prince-Sharma-07@users.noreply.github.com>
2025-07-10 10:09:48 +0530

    Time: 0 ms (100%), Space: 26.3 MB (33.69%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int Xor = 0;
+        for(auto i : nums){
+            Xor ^= i;
+        }
+        return Xor;
+    }
+};
