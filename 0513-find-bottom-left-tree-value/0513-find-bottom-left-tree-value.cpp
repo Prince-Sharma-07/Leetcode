a63b826d952342a20c966e1c3f75cb6bccf6191e
Prince sharma <131739429+Prince-Sharma-07@users.noreply.github.com>
2025-07-12 10:28:41 +0530

    Time: 0 ms (100%), Space: 25 MB (46.06%) - LeetHub

---
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        queue<TreeNode*> q;
+        int ans = -1;
+        q.push(root);
+        while(!q.empty()){
+            int n = q.size();
+            ans = q.front()->val;
+            while(n--){
+            TreeNode* temp = q.front();
+            q.pop();
+            if(temp->left) q.push(temp->left);
+            if(temp->right) q.push(temp->right);
+            }
+        }
+        return ans;
+    }
+};
