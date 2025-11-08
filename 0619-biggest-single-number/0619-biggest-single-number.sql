4c589d6c753f621073e82757cdea896e060ef98e
Prince sharma <131739429+Prince-Sharma-07@users.noreply.github.com>
2025-11-08 19:09:51 +0530

    Time: 690 ms (5.48%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,7 +1,9 @@
 # Write your MySQL query statement below
 
+with SingleNum as (
+    select num from Mynumbers 
+    group by num
+    having count(num) = 1
+)
 
-select max(num) as num from (select num from MyNumbers
-group by num
-having count(num) = 1
-) as t
+select max(num) as num from SingleNum
